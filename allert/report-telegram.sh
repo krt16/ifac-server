d6b27994d3b2593fdaf21260a9f48e4d0e09c7a9
Ragil Alfajri <87970975+krt16@users.noreply.github.com>
2022-07-19 11:41:56 +0800

    Update report-telegram.sh

--- allert/report-telegram.sh
+++ allert/report-telegram.sh
@@ -9,13 +9,13 @@ ping -c 13 $TARGET > /dev/null
 if [ $? != 1 ]
 
 then
-  echo "VPN H3I-NG AETRA Down  '$NOW'" >> $LOG
+  echo "VPN Down  '$NOW'" >> $LOG
 #ipsec restart
-  echo "VPN H3I-NG AETRA Down dan udah direstart '$NOW'" >> $LOG
+  echo "VPN  Down dan udah direstart '$NOW'" >> $LOG
   curl $SA
-  #mail -s "VPN AETRA!" infra@bsa.id < $LOG
+  #mail -s "VPN!" email@.com < $LOG
 
 else
-  echo "VPN H3I-NG AETRA baik-baik saja '$NOW'" >> $LOG
+  echo "VPN baik-baik saja '$NOW'" >> $LOG
 
 fi
